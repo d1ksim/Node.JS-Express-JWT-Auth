ad88cab71d7f93d6eea4d5e400bddd86c4936c74
d1mpi <d1mpi.developer@gmail.com>
2021-08-31 21:26:49 +0300

    Изменена структура приложения

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import helmet from 'helmet';
-//import {authRouter} from './router/auth.js';
 import { router } from './router/index.js'
 import bodyParser from "body-parser";
 import { config } from 'dotenv';
@@ -15,6 +14,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api', router);
-//app.use('/api/auth', authRouter);
 
-app.listen(process.env.PORT, () => { console.log('Проект запущен'); });
+app.listen(process.env.PORT, () => { console.log(`Server started on port: ${process.env.PORT}`); });
